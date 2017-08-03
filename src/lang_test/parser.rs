c0ac0bd61fdb5a9c1e7addf6914d899468eab634
LarryLo <larry.lo1025@gmail.com>
2017-08-03 06:34:49 +0000

    robust error handling in python parser

--- src/lang_test/parser.rs
+++ src/lang_test/parser.rs
@@ -1,5 +1,6 @@
 extern crate regex;
 use self::regex::Regex;
+use self::regex::Captures;
 
 #[derive(RustcEncodable, RustcDecodable)]
 pub struct ResponseMetrics {
@@ -12,28 +13,28 @@ pub fn parse_py_rsp(text: &str) -> ResponseMetrics {
 
     let re_fail = Regex::new(r"Ran (\d) tests(.+|\n)=(\d)").unwrap(); 
     let re_success = Regex::new(r"Ran (\d) tests(.+|\n)(OK)").unwrap(); 
-    let mut cap;
+    let mut cap: Captures;
 
-    if re_fail.is_match(text) {
-        cap = re_fail.captures(text).unwrap(); 
-
-    } else {
-        cap = re_success.captures(text).unwrap();
-    }
-    println!("{:?}", cap);
-
-    match cap.get(3).map_or("", |m| m.as_str()) {
-        "OK" => {
-            let total = cap.get(1).map_or("", |m| m.as_str()).parse().unwrap();
-            println!("total: {}, error: 0, success: {}", total, total);
-            ResponseMetrics { total: total, error: 0, success: total}
-        },
-        _ => { 
-            let total = cap.get(1).map_or("", |m| m.as_str()).parse().unwrap();
-            let error = cap.get(3).map_or("", |m| m.as_str()).parse().unwrap();
+    match re_fail.captures(text) {
+        Some(cap) => {
+            let total = cap.get(1).unwrap().as_str().parse().unwrap();
+            let error = cap.get(3).unwrap().as_str().parse().unwrap();
             println!("total: {}, error: {}, success: 0", total, error);
             ResponseMetrics { total: total, error: error, success: 0 }
-        }, 
+        },
+        None => {
+            match re_success.captures(text) {
+                Some(cap) => {
+                    let total = cap.get(1).unwrap().as_str().parse().unwrap(); 
+                    println!("total: {}, error: 0, success: {}", total, total);
+                    ResponseMetrics { total: total, error: 0, success: total}
+                },
+                None => {
+                    ResponseMetrics { total: 0, error: 1, success: 0 }
+
+                }
+            }
+        } 
     }
 }
 
